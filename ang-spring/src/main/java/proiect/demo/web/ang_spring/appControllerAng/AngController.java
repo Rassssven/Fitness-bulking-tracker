9a3c9a2a4172b818df4816606831cd6b6b62564e
Rassssven <dtimonea@gmail.com>
2026-02-27 16:01:45 +0200

    Log4j

--- ang-spring/src/main/java/proiect/demo/web/ang_spring/appControllerAng/AngController.java
+++ ang-spring/src/main/java/proiect/demo/web/ang_spring/appControllerAng/AngController.java
@@ -2,6 +2,8 @@ package proiect.demo.web.ang_spring.appControllerAng;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class AngController {
 
+	private static final Logger logger = LogManager.getLogger(AngController.class);
+
     @GetMapping("/hello")
     public String hello() {
         return "Salut din Spring Boot!";
     }
+    
+    @GetMapping("/testLog4j")
+    public String test() {
+    	logger.info("Logger apelat");
+    	logger.debug("Debug info");
+    	logger.error("Test error log");
+    	
+    	return "Ok!";
+    }
+    
+    
 }
