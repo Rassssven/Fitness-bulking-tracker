c75c9c6bb763c383068581b1c6af0accc317bee8
Rassssven <dtimonea@gmail.com>
2026-04-03 19:45:25 +0300

    Exercise Entity

---
+++ ang-spring/src/main/java/proiect/demo/web/ang_spring/Entities/Exercises.java
@@ -0,0 +1,86 @@
+package proiect.demo.web.ang_spring.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Exercises")
+public class Exercises {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private int type;
+	private int caloriesPerExercise;
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Exercises(String name, int type, int caloriesPerExercise, String description) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.caloriesPerExercise = caloriesPerExercise;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getCaloriesPerExercise() {
+		return caloriesPerExercise;
+	}
+
+	public void setCaloriesPerExercise(int caloriesPerExercise) {
+		this.caloriesPerExercise = caloriesPerExercise;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+}
