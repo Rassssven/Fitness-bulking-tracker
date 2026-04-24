05f3e253bd523bfe9fa5a2227b8616e3c015d4f7
Rassssven <dtimonea@gmail.com>
2026-04-24 21:07:02 +0300

    Service

---
+++ ang-spring/src/main/java/proiect/demo/web/ang_spring/Services/ExerciseService.java
@@ -0,0 +1,16 @@
+package proiect.demo.web.ang_spring.Services;
+
+import proiect.demo.web.ang_spring.db.ExerciseRepository;
+
+public class ExerciseService {
+
+	private final ExerciseRepository exerRepo;
+
+	public ExerciseService(ExerciseRepository exerRepo) {
+		super();
+		this.exerRepo = exerRepo;
+	}
+	
+	
+	
+}
