1de1e0c7bfc19e019e75637f579c99166082b7ac
Rassssven <dtimonea@gmail.com>
2026-04-26 18:04:35 +0300

    Refactor - calc component

--- angFrontend/src/app/app.routes.ts
+++ angFrontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Shop } from './pages/shop/shop';
-import { ProductDetails } from './products/product-details/product-details';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'shop', component: Shop },
-    { path: 'product/:id', component: ProductDetails }
 ];
