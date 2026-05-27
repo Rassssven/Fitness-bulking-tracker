2fcf9728a1c1db7f1716092e52cfc012fdd559f3
Rassssven <dtimonea@gmail.com>
2026-05-28 02:50:42 +0300

    XML Security

--- ang-spring/src/main/java/proiect/demo/web/ang_spring/AngSpringApplication.java
+++ ang-spring/src/main/java/proiect/demo/web/ang_spring/AngSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ImportResource("classpath:security-context.xml")
 public class AngSpringApplication {
 
 	public static void main(String[] args) {
