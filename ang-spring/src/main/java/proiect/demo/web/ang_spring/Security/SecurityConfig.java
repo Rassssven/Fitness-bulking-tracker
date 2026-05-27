48313187961c54631c4276a9557eff494d445514
Rassssven <dtimonea@gmail.com>
2026-05-27 20:35:07 +0300

    JWT

--- ang-spring/src/main/java/proiect/demo/web/ang_spring/Security/SecurityConfig.java
+++ ang-spring/src/main/java/proiect/demo/web/ang_spring/Security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package proiect.demo.web.ang_spring.Security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,10 +15,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import proiect.demo.web.ang_spring.Security.JWT.JWTFilter;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
 
     private final JWTFilter jwtFilter;
@@ -50,12 +55,33 @@ public class SecurityConfig {
             throws Exception {
         return config.getAuthenticationManager();
     }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+
+        configuration.setAllowedMethods(List.of("*"));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
+            .cors(cors -> {})
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
