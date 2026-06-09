73b1134c146c06f263287215785a030cc4244271
Rassssven <dtimonea@gmail.com>
2026-06-09 20:48:42 +0300

    Entities UI

--- angFrontend/src/app/app.routes.ts
+++ angFrontend/src/app/app.routes.ts
@@ -12,6 +12,8 @@ import { ProductCard } from './Shop/product-card/product-card';
 import { DiagramPage } from './pages/diagram-page/diagram-page';
 import { authGuard } from './core/guards/auth-guard';
 import { AdminDash } from './pages/admin-dash/admin-dash';
+import { FoodCatalog } from './pages/other_pages/food-catalog/food-catalog';
+import { ExerciseCatalog } from './pages/other_pages/exercise-catalog/exercise-catalog';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -28,5 +30,7 @@ export const routes: Routes = [
     { path: 'login', component: Login},
     { path: 'register', component: Register},
     { path: 'diagram', component: DiagramPage },
-    { path: 'admin', component: AdminDash }
+    { path: 'admin', component: AdminDash },
+    { path: 'food-catalog', component: FoodCatalog },
+    { path: 'exercise-catalog', component: ExerciseCatalog }
 ];
---
+++ angFrontend/src/app/pages/other_pages/exercise-catalog/exercise-catalog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-catalog',
+  imports: [],
+  templateUrl: './exercise-catalog.html',
+  styleUrl: './exercise-catalog.css',
+})
+export class ExerciseCatalog {
+
+}
