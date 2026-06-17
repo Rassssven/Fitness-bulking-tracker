d3dfd6cc06af8ae24c381aa837d393fdebd7c418
Rassssven <dtimonea@gmail.com>
2026-06-17 17:59:51 +0300

    Reactive Forms Added

--- angFrontend/src/app/pages/other_pages/food-catalog/food-catalog.ts
+++ angFrontend/src/app/pages/other_pages/food-catalog/food-catalog.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { FoodService } from '../../../services/HTTP/food-service';
-import { CreateFoodRequest } from '../../../models/DTO/CreateFoodRequest';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Meal } from '../../../models/meal';
 import { NotificationService } from '../../../shared/notification-service';
+import { CreateFoodRequest } from '../../../models/DTO/CreateFoodRequest';
 
 @Component({
   selector: 'app-food-catalog',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './food-catalog.html',
   styleUrl: './food-catalog.css',
 })
@@ -18,14 +18,30 @@ export class FoodCatalog implements OnInit {
 
   foods = signal<Meal[]>([]);
 
-  foodData: CreateFoodRequest = {
-    name: '',
-    calories: 0,
-    protein: 0,
-    carbs: 0,
-    fat: 0,
-    description: ''
-  };
+  foodForm = new FormGroup({
+
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+
+    calories: new FormControl(0, [
+      Validators.required
+    ]),
+
+    protein: new FormControl(0, [
+      Validators.required
+    ]),
+
+    carbs: new FormControl(0, [
+      Validators.required
+    ]),
+
+    fat: new FormControl(0),
+
+    description: new FormControl('')
+
+  });
 
   showFoodForm = false;
 
@@ -41,9 +57,35 @@ export class FoodCatalog implements OnInit {
 
   createFood() {
 
-    this.foodServ.createFood(this.foodData).subscribe({
+    if(this.foodForm.invalid) {
+      return;
+    }
+
+    const dto: CreateFoodRequest = {
+      name: this.foodForm.value.name ?? '',
+      calories: this.foodForm.value.calories ?? 0,
+      protein: this.foodForm.value.protein ?? 0,
+      carbs: this.foodForm.value.carbs ?? 0,
+      fat: this.foodForm.value.fat ?? 0,
+      description: this.foodForm.value.description ?? ''
+    }
+
+    this.foodServ.createFood(dto).subscribe({
       next: food => {
-        console.log(food);
+        
+        this.foods.update(foods => [
+          ...foods, food
+        ]);
+
+        this.foodForm.reset({
+          calories: 0,
+          protein: 0,
+          carbs: 0,
+          fat: 0
+        });
+
+        this.showFoodForm = false;
+
         this.notifService.showSuccess("Food added!");
       }
     });
